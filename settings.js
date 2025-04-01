37ddd6eb3221323e643ad59bb1f2f99a59a64f3b
thashmila-sl <tgamith2@gmail.com>
2025-04-01 13:28:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Thashmila',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Thashmila Bot",
+  botOwner: 'Thashmila', // Your name
+  ownerNumber: '94762736725', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
